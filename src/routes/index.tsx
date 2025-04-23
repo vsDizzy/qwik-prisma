298fc8a960130c14029602acf25bb541b41c6a03
Dizzy <todizzy@gmail.com>
2025-04-23 03:18:57 +0300

    added routeLoader$ - builds fine

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,17 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik'
+import { routeLoader$, type DocumentHead } from '@builder.io/qwik-city'
+import { PrismaClient } from '~/../prisma/generated'
+
+export const useUsers = routeLoader$(async () => {
+  const prisma = new PrismaClient()
+  return await prisma.user.findMany()
+})
+
+const UserList = component$(() => {
+  const users = useUsers()
+
+  return <>Users count: {users.value.length}</>
+})
 
 export default component$(() => {
   return (
@@ -10,16 +22,17 @@ export default component$(() => {
         <br />
         Happy coding.
       </div>
+      <UserList />
     </>
-  );
-});
+  )
+})
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: 'Welcome to Qwik',
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: 'description',
+      content: 'Qwik site description',
     },
   ],
-};
+}
