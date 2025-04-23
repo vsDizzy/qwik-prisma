44b80525aa6a5b3fc154a5fa0e1ef3f168f6f1d8
Dizzy <todizzy@gmail.com>
2025-04-23 03:28:21 +0300

    added formAction$ - build error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,52 @@
-import { component$ } from '@builder.io/qwik'
+import { $, component$ } from '@builder.io/qwik'
 import { routeLoader$, type DocumentHead } from '@builder.io/qwik-city'
+import { formAction$, useForm, type InitialValues } from '@modular-forms/qwik'
 import { PrismaClient } from '~/../prisma/generated'
+
+type LoginForm = {
+  email: string
+  password: string
+}
+
+export const useLoginFormLoader = routeLoader$<InitialValues<LoginForm>>(() => {
+  return { email: '', password: '' }
+})
+
+const useFormAction = formAction$<LoginForm>(
+  async (data) => {
+    console.log('Form data:', data)
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const prisma = new PrismaClient()   //  <-- This line breaks the build. Comment it to check.
+    // const users = await prisma.user.findMany()
+    // console.log('Users:', users)
+  },
+  $(() => ({}))
+)
+
+export const LoginFormComponent = component$(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [loginForm, { Form, Field }] = useForm<LoginForm>({
+    loader: useLoginFormLoader(),
+    action: useFormAction(),
+  })
+
+  return (
+    <Form>
+      <Field name="email">
+        {(field, props) => (
+          <input {...props} type="email" value={field.value} />
+        )}
+      </Field>
+      <Field name="password">
+        {(field, props) => (
+          <input {...props} type="password" value={field.value} />
+        )}
+      </Field>
+      <button type="submit">Login</button>
+    </Form>
+  )
+})
 
 export const useUsers = routeLoader$(async () => {
   const prisma = new PrismaClient()
@@ -23,6 +69,8 @@ export default component$(() => {
         Happy coding.
       </div>
       <UserList />
+      <br />
+      <LoginFormComponent />
     </>
   )
 })
